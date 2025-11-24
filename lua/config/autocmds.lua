3db48992208b508ca51c6c4cfe4e4dec5313a56a
Thanhmay2406 <Thanhmay2406@gmail.com>
2025-11-24 20:41:51 +0700

    update new plugins for c++

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_user_command("CP", function()
+  local template = vim.fn.expand("~/.config/nvim/templates/cp_template.cpp")
+  vim.cmd("0r " .. template)
+  vim.api.nvim_win_set_cursor(0, { 45, 0 })
+end, {})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.tabstop = 4 -- Số lượng khoảng trắng cho 1 tab
+vim.opt.shiftwidth = 4 -- Số lượng khoảng trắng khi thụt đầu dòng (indent)
+vim.opt.expandtab = true -- Chuyển đổi tab thành khoảng trắng
+vim.opt.softtabstop = 4 -- Số lượng khoảng trắng khi nhấn Tab hoặc Backspace
+
+-- tắt hiệu ứng con trỏ chuột
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_size = 0
