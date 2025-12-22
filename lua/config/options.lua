8ab675db84465831fad681e6cf00b80ae204698b
Thanhmay2406 <thanhmay2406@gmail.com>
2025-12-22 09:53:11 +0700

    update: new version

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.tabstop = 4 -- Số lượng khoảng trắng cho 1 tab
-vim.opt.shiftwidth = 4 -- Số lượng khoảng trắng khi thụt đầu dòng (indent)
+vim.opt.tabstop = 2 -- Số lượng khoảng trắng cho 1 tab
+vim.opt.shiftwidth = 2 -- Số lượng khoảng trắng khi thụt đầu dòng (indent)
 vim.opt.expandtab = true -- Chuyển đổi tab thành khoảng trắng
-vim.opt.softtabstop = 4 -- Số lượng khoảng trắng khi nhấn Tab hoặc Backspace
+vim.opt.softtabstop = 2 -- Số lượng khoảng trắng khi nhấn Tab hoặc Backspace
 
 -- tắt hiệu ứng con trỏ chuột
 vim.g.neovide_cursor_animation_length = 0
